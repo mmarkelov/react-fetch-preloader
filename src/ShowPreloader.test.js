da1e4b7c97c70609fa490f071d4c9f43a3e344be
mapkeji <mapkeji26@gmail.com>
2018-12-10 19:56:23 +0300

    Add basic ShowPreloader tests

--- src/ShowPreloader.test.js
+++ src/ShowPreloader.test.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import 'isomorphic-fetch';
 import { shallow } from 'enzyme';
 import ShowPreloader from './ShowPreloader';
 
 const Loaded = () => <div>Loaded</div>;
 const URL = 'https://randomuser.me/api/?results=10';
 
-describe('ShowPreloader', () => {
-  it('should render correctly with all props', () => {
-    const component = shallow(<ShowPreloader url={URL} loaded={Loaded} />);
-    expect(component).toMatchSnapshot();
-  });
+const fetch = jest.fn().mockReturnValue(Promise.resolve());
+const rejectedFetch = jest.fn().mockReturnValue(Promise.reject('Error'));
 
+describe('ShowPreloader', () => {
   it('should throw Error without url prop', () => {
     try {
       shallow(<ShowPreloader loaded={Loaded} />);
     } catch (e) {
-      expect(e.message).toEqual('url param is required!')
+      expect(e.message).toEqual('url param is required!');
     }
   });
 
@@ -24,9 +21,27 @@ describe('ShowPreloader', () => {
     try {
       shallow(<ShowPreloader url={URL} />);
     } catch (e) {
-      expect(e.message).toEqual('loaded param is required!')
+      expect(e.message).toEqual('loaded param is required!');
     }
+  });
+
+  it('should render error component if response failed', async () => {
+    const component = shallow(
+      <ShowPreloader url={URL} loaded={Loaded} fetch={rejectedFetch} />,
+    );
+    await component.instance().makeResponse();
+    process.nextTick(() => {
+      component.update();
+      expect(component.html()).toBe('<span>ERROR</span>');
+    });
+  });
+
+  it('should render correctly with all props', () => {
+    const component = shallow(
+      <ShowPreloader url={URL} loaded={Loaded} fetch={fetch} />,
+    );
+    expect(component).toMatchSnapshot();
   });
 });
 
-afterAll(() => setTimeout(() => process.exit(), 1000))
+afterAll(() => setTimeout(() => process.exit(), 1000));
