b665acdc53dd09f16a19365465780cc971a43aed
mapkeji <mapkeji26@gmail.com>
2018-12-12 11:17:31 +0300

    Add withPreloader HOC tests

--- src/withPreloader.test.js
+++ src/withPreloader.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import withPreloader from './withPreloader';
+import ShowPreloader from './ShowPreloader';
 
 const Loaded = () => <div>Loaded</div>;
 const URL = 'https://randomuser.me/api/?results=10';
@@ -30,10 +31,39 @@ class App extends React.Component {
 
 const WrappedApp = withPreloader(App);
 
-describe('withPreloader HOC component', () => {
+describe('withPreloader HOC', () => {
   it('should render correctly with all props', () => {
     const component = shallow(<WrappedApp />);
     expect(component).toMatchSnapshot();
+  });
+
+  it('preload function should throw Error without id param', () => {
+    const component = shallow(<WrappedApp />);
+    try {
+      component.instance().preload();
+    } catch (e) {
+      expect(e.message).toBe('Id param is required!');
+    }
+  });
+
+  it('preload function should throw Error without url param', () => {
+    const component = shallow(<WrappedApp />);
+    try {
+      component.instance().preload(1);
+    } catch (e) {
+      expect(e.message).toBe('Url param is required!');
+    }
+  });
+
+  it('renderPreloader function should render ShowPreloader component', () => {
+    const component = shallow(<WrappedApp />);
+    component.setState({
+      test: { url: 'test_url' },
+      showPreloaderOptions: { url: URL, loaded: Loaded },
+    });
+    expect(component.instance().renderPreloader('test')).toEqual(
+      <ShowPreloader loaded={Loaded} url={URL} />,
+    );
   });
 });
 
